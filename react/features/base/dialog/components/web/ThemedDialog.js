c7d4bfcdc966df110a5f38da28e1b0e02b490410
tlests <tlests@hotmail.com>
2021-06-15 16:58:54 +0700

    revert

--- react/features/base/dialog/components/web/ThemedDialog.js
+++ react/features/base/dialog/components/web/ThemedDialog.js
@@ -3,34 +3,23 @@ import {
     FillScreen,
     dialogWidth,
     dialogHeight,
-    // PositionerAbsolute,
-    // PositionerRelative
-} from "@atlaskit/modal-dialog/dist/es2019/styled/Modal.js";
-import { N0, DN50 } from "@atlaskit/theme/colors";
-import { themed } from "@atlaskit/theme/components";
-import React from "react";
+    PositionerAbsolute,
+    PositionerRelative
+} from '@atlaskit/modal-dialog/dist/es2019/styled/Modal.js';
+import { N0, DN50 } from '@atlaskit/theme/colors';
+import { themed } from '@atlaskit/theme/components';
+import React from 'react';
 
-const ThemedDialog = (props) => {
-    const style = {
-        backgroundColor: props.isChromeless
-            ? "transparent"
-            : themed({ light: N0, dark: DN50 })({ theme: { mode: "dark" } }),
-    };
+const ThemedDialog = props => {
+    const style = { backgroundColor: props.isChromeless ? 'transparent' : themed({ light: N0,
+        dark: DN50 })({ theme: { mode: 'dark' } }) };
 
-    return (
-        <Dialog
-            {...props}
-            aria-modal={true}
-            style={style}
-            theme={{ mode: "dark" }}
-        />
-    );
+    return (<Dialog
+        { ...props }
+        aria-modal = { true }
+        style = { style }
+        theme = {{ mode: 'dark' }} />);
 };
 
-export {
-    ThemedDialog as Dialog,
-    FillScreen,
-    dialogWidth,
-    dialogHeight,
-    // PositionerAbsolute, PositionerRelative
-};
+
+export { ThemedDialog as Dialog, FillScreen, dialogWidth, dialogHeight, PositionerAbsolute, PositionerRelative };
