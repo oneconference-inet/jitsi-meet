dd637fb6cf02d5fc713a714cf96f377ea4dec1a5
tlests <tlests@hotmail.com>
2021-06-23 16:18:09 +0700

    add / in import file

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -12,7 +12,7 @@ import type { AbstractButtonProps } from "../../base/toolbox/components";
 import { getLocalParticipant, PARTICIPANT_ROLE } from "../../base/participants";
 import { getActiveSession } from "../../recording/functions";
 import { JitsiRecordingConstants } from "../../base/lib-jitsi-meet";
-import { EndMeetingDialog } from "../../video-menu/components";
+import { EndMeetingDialog } from "../../video-menu/components/";
 import { openDialog } from "../../base/dialog";
 
 import axios from "axios";
