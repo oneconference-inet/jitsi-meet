d177cbac890732e0e74157d0ced4fbfcddf8171c
tlests <tlests@hotmail.com>
2021-06-23 15:42:04 +0700

    change path

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -9,12 +9,18 @@ import { translate } from "../../base/i18n";
 import { connect } from "../../base/redux";
 import { AbstractHangupButton } from "../../base/toolbox/components";
 import type { AbstractButtonProps } from "../../base/toolbox/components";
+import { getLocalParticipant, PARTICIPANT_ROLE } from "../../base/participants";
+import { getActiveSession } from "../../recording/functions";
+import { JitsiRecordingConstants } from "../../base/lib-jitsi-meet";
+import { EndMeetingDialog } from "../../remote-video-menu/components";
+import { openDialog } from "../../base/dialog";
 
 import axios from "axios";
 import infoConf from "../../../../infoConference";
 import infoUser from "../../../../infoUser";
 
 declare var interfaceConfig: Object;
+declare var APP: Object;
 
 /**
  * The type of the React {@code Component} props of {@link HangupButton}.
@@ -47,151 +53,47 @@ class HangupButton extends AbstractHangupButton<Props, *> {
     constructor(props: Props) {
         super(props);
 
-        this._hangup = _.once(() => {
-            sendAnalytics(createToolbarEvent("hangup"));
-            this._endJoin();
+        this._hangup = () => {
+            // sendAnalytics(createToolbarEvent("hangup"));
+            // this._endJoin();
+
             // FIXME: these should be unified.
             if (navigator.product === "ReactNative") {
                 this.props.dispatch(appNavigate(undefined));
             } else {
-                this.props.dispatch(disconnect(true));
-            }
-        });
-
-        this._endJoin = async () => {
-            const domainEnd = interfaceConfig.DOMAIN_BACK;
-            const service = infoConf.getService();
-            const meetingId = infoConf.getMeetingId();
-            const isModerator = infoConf.getIsModerator();
-            const nameJoin = infoUser.getName();
-            const userId = infoUser.getUserId();
-            const secretKeyManageAi = interfaceConfig.SECRET_KEY_MANAGE_AI;
-            const secretKeyOnechat = interfaceConfig.SECRET_KEY_ONECHAT;
-            const secretKeyOneDental = interfaceConfig.SECRET_KEY_ONE_DENTAL;
-            const secretKeyOneBinar = interfaceConfig.SECRET_KEY_ONE_BINAR;
-            const secretKeyJmc = interfaceConfig.SECRET_KEY_JMC;
-            const secretKeyTelemedicine = interfaceConfig.SECRET_KEY_TELEMEDICINE;
-            if (isModerator) {
-                infoConf.setIsHostHangup();
-            }
-
-            if (service == "onechat") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "onechat",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyOnechat 
-                        }
-                    }
-                );
-            } else if (service == "manageAi") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "ManageAi",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyManageAi 
-                        }
-                    }
-                );
-            } else if (service == "onemail") {
+                const { dispatch, localParticipantId, isModerator } =
+                    this.props;
                 if (isModerator) {
-                    await axios.post(
-                        interfaceConfig.DOMAIN_ONEMAIL +
-                        "/api/v1/oneconf/service/hangup",
-                        {
-                            meeting_id: meetingId,
-                            user_id: userId,
-                            tag: "onemail",
-                        }
+                    var state = APP.store.getState();
+                    const _fileRecordingSessionOn = Boolean(
+                        getActiveSession(
+                            state,
+                            JitsiRecordingConstants.mode.FILE
+                        )
                     );
-                } 
-                else {
-                    await axios.post(
-                        interfaceConfig.DOMAIN_ONEMAIL +
-                        "/api/v1/oneconf/service/hangup",
-                        {
-                            meeting_id: meetingId,
-                            user_id: userId,
-                            tag: "onemail",
-                        }
+
+                    if (_fileRecordingSessionOn) {
+                        const _conference =
+                            state["features/base/conference"].conference;
+                        const _fileRecordingSession = getActiveSession(
+                            state,
+                            JitsiRecordingConstants.mode.FILE
+                        );
+                        _conference.stopRecording(_fileRecordingSession.id);
+                    }
+
+                    sendAnalytics(createToolbarEvent("endmeeting.pressed"));
+                    dispatch(
+                        openDialog(EndMeetingDialog, {
+                            exclude: [localParticipantId],
+                        })
                     );
+                } else {
+                    _endJoin()
                 }
-            } else if (service == "onemail_dga") {
-                await axios.post(interfaceConfig.DOMAIN_ONEMAIL_DGA + "/endJoin", 
-                    {
-                        user_id: userId.split('-')[0],
-                        meeting_id: meetingId,
-                    }
-                );
-            } else if (service == "onedental") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "onedental",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyOneDental 
-                        }
-                    }
-                );
-            } else if (service == "onebinar") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "onebinar",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyOneBinar 
-                        }
-                    }
-                );
-            } else if (service == "jmc") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "jmc",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyJmc 
-                        }
-                    }
-                );
-            } else if (service == "telemedicine") {
-                await axios.post(domainEnd + "/service/endjoin", 
-                    {
-                        meetingid: meetingId,
-                        name: nameJoin,
-                        tag: "telemedicine",
-                    },
-                    { headers:
-                        {
-                        Authorization: "Bearer " + secretKeyTelemedicine 
-                        }
-                    }
-                );
-            } else {
-                await axios.post(interfaceConfig.DOMAIN + "/endJoin", {
-                    user_id: userId,
-                    meeting_id: meetingId,
-                });
             }
         };
     }
-
     /**
      * Helper function to perform the actual hangup action.
      *
@@ -204,4 +106,181 @@ class HangupButton extends AbstractHangupButton<Props, *> {
     }
 }
 
-export default translate(connect()(HangupButton));
+/**
+ * Maps part of the redux state to the component's props.
+ *
+ * @param {Object} state - The redux store/state.
+ * @param {Props} ownProps - The component's own props.
+ * @returns {Object}
+ */
+function _mapStateToProps(state: Object, ownProps: Props) {
+    const localParticipant = getLocalParticipant(state);
+    const isModerator = localParticipant.role === PARTICIPANT_ROLE.MODERATOR;
+    // const { visible } = ownProps;
+    // const { disableRemoteMute } = state["features/base/config"];
+
+    return {
+        isModerator,
+        localParticipantId: localParticipant.id,
+        visible: true,
+    };
+}
+
+export default translate(connect(_mapStateToProps)(HangupButton));
+
+export async function _endJoin() {
+    try {
+        const domainEnd = interfaceConfig.DOMAIN_BACK;
+        const service = infoConf.getService();
+        const meetingId = infoConf.getMeetingId();
+        const isModerator = infoConf.getIsModerator();
+        const nameJoin = infoUser.getName();
+        const userId = infoUser.getUserId();
+        const secretKeyManageAi = interfaceConfig.SECRET_KEY_MANAGE_AI;
+        const secretKeyOnechat = interfaceConfig.SECRET_KEY_ONECHAT;
+        const secretKeyOneDental = interfaceConfig.SECRET_KEY_ONE_DENTAL;
+        const secretKeyOneBinar = interfaceConfig.SECRET_KEY_ONE_BINAR;
+        const secretKeyJmc = interfaceConfig.SECRET_KEY_JMC;
+        const secretKeyTelemedicine = interfaceConfig.SECRET_KEY_TELEMEDICINE;
+        const secretKeyEmeeting = interfaceConfig.SECRET_KEY_EMEETING;
+        if (isModerator) {
+            infoConf.setIsHostHangup();
+        }
+
+        if (service == "onechat") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "onechat",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyOnechat,
+                    },
+                }
+            );
+        } else if (service == "manageAi") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "ManageAi",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyManageAi,
+                    },
+                }
+            );
+        } else if (service == "onemail") {
+            if (isModerator) {
+                await axios.post(
+                    interfaceConfig.DOMAIN_ONEMAIL +
+                        "/api/v1/oneconf/service/hangup",
+                    {
+                        meeting_id: meetingId,
+                        user_id: userId,
+                        tag: "onemail",
+                    }
+                );
+            } else {
+                await axios.post(
+                    interfaceConfig.DOMAIN_ONEMAIL +
+                        "/api/v1/oneconf/service/hangup",
+                    {
+                        meeting_id: meetingId,
+                        user_id: userId,
+                        tag: "onemail",
+                    }
+                );
+            }
+        } else if (service == "onemail_dga") {
+            await axios.post(interfaceConfig.DOMAIN_ONEMAIL_DGA + "/endJoin", {
+                user_id: userId.split("-")[0],
+                meeting_id: meetingId,
+            });
+        } else if (service == "onedental") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "onedental",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyOneDental,
+                    },
+                }
+            );
+        } else if (service == "onebinar") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "onebinar",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyOneBinar,
+                    },
+                }
+            );
+        } else if (service == "jmc") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "jmc",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyJmc,
+                    },
+                }
+            );
+        } else if (service == "telemedicine") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "telemedicine",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyTelemedicine,
+                    },
+                }
+            );
+        } else if (service == "emeeting") {
+            await axios.post(
+                domainEnd + "/service/endjoin",
+                {
+                    meetingid: meetingId,
+                    name: nameJoin,
+                    tag: "emeeting",
+                },
+                {
+                    headers: {
+                        Authorization: "Bearer " + secretKeyEmeeting,
+                    },
+                }
+            );
+        } else {
+            await axios.post(interfaceConfig.DOMAIN + "/endJoin", {
+                user_id: userId,
+                meeting_id: meetingId,
+            });
+        }
+
+        APP.store.dispatch(disconnect(true));
+    } catch (error) {
+        console.log(error);
+    }
+}
