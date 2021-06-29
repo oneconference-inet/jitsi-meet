c2fabd43e32d8e6bd6a9c44006159207af4f237f
tlests <tlests@hotmail.com>
2021-06-29 15:02:15 +0700

    remove log toolbarbut

--- react/features/base/config/functions.web.js
+++ react/features/base/config/functions.web.js
@@ -42,6 +42,6 @@ export function getDialOutUrl(state: Object): string {
  */
 export function getToolbarButtons(state: Object): Array<string> {
     const { toolbarButtons } = state["features/base/config"];
-    console.log("toolbarbut: ", state);
+
     return Array.isArray(toolbarButtons) ? toolbarButtons : TOOLBAR_BUTTONS;
 }
