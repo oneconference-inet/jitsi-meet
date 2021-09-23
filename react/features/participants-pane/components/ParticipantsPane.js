37c676ba1f57d301ac90418398792ac6ce1b0637
tlests <tlests@hotmail.com>
2021-09-23 16:53:03 +0700

    log isLocalModerator

--- react/features/participants-pane/components/ParticipantsPane.js
+++ react/features/participants-pane/components/ParticipantsPane.js
@@ -1,27 +1,27 @@
 // @flow
 
-import React, { useCallback } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
+import React, { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useDispatch, useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
 
-import { openDialog } from '../../base/dialog';
-import { isLocalParticipantModerator } from '../../base/participants';
-import { MuteEveryoneDialog } from '../../video-menu/components/';
-import { close } from '../actions';
-import { classList, getParticipantsPaneOpen } from '../functions';
-import theme from '../theme.json';
+import { openDialog } from "../../base/dialog";
+import { isLocalParticipantModerator } from "../../base/participants";
+import { MuteEveryoneDialog } from "../../video-menu/components/";
+import { close } from "../actions";
+import { classList, getParticipantsPaneOpen } from "../functions";
+import theme from "../theme.json";
 
-import { LobbyParticipantList } from './LobbyParticipantList';
-import { MeetingParticipantList } from './MeetingParticipantList';
+import { LobbyParticipantList } from "./LobbyParticipantList";
+import { MeetingParticipantList } from "./MeetingParticipantList";
 import {
     AntiCollapse,
     Close,
     Container,
     Footer,
     FooterButton,
-    Header
-} from './styled';
+    Header,
+} from "./styled";
 
 export const ParticipantsPane = () => {
     const dispatch = useDispatch();
@@ -29,19 +29,24 @@ export const ParticipantsPane = () => {
     const isLocalModerator = useSelector(isLocalParticipantModerator);
     const { t } = useTranslation();
 
-    const closePane = useCallback(() => dispatch(close(), [ dispatch ]));
-    const muteAll = useCallback(() => dispatch(openDialog(MuteEveryoneDialog)), [ dispatch ]);
+    const closePane = useCallback(() => dispatch(close(), [dispatch]));
+    const muteAll = useCallback(
+        () => dispatch(openDialog(MuteEveryoneDialog)),
+        [dispatch]
+    );
 
     return (
-        <ThemeProvider theme = { theme }>
+        <ThemeProvider theme={theme}>
+            {console.log("isLocalModerator: ", isLocalModerator)}
             <div
-                className = { classList(
-          'participants_pane',
-          !paneOpen && 'participants_pane--closed'
-                ) }>
-                <div className = 'participants_pane-content'>
+                className={classList(
+                    "participants_pane",
+                    !paneOpen && "participants_pane--closed"
+                )}
+            >
+                <div className="participants_pane-content">
                     <Header>
-                        <Close onClick = { closePane } />
+                        <Close onClick={closePane} />
                     </Header>
                     <Container>
                         <LobbyParticipantList />
@@ -50,8 +55,8 @@ export const ParticipantsPane = () => {
                     </Container>
                     {isLocalModerator && (
                         <Footer>
-                            <FooterButton onClick = { muteAll }>
-                                {t('participantsPane.actions.muteAll')}
+                            <FooterButton onClick={muteAll}>
+                                {t("participantsPane.actions.muteAll")}
                             </FooterButton>
                         </Footer>
                     )}
