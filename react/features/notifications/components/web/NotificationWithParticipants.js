c21d866f97caa6657dc2f84bab72bc34d657e458
MaMookMik <mookjrtv3@gmail.com>
2021-09-30 21:32:26 +0700

    test old knocking participants

--- react/features/notifications/components/web/NotificationWithParticipants.js
+++ react/features/notifications/components/web/NotificationWithParticipants.js
@@ -7,6 +7,8 @@ import { HIDDEN_EMAILS } from '../../../lobby/constants';
 
 import NotificationButton from './NotificationButton';
 
+import infoConf from "../../../../../infoConference";
+
 type Props = {
 
     /**
@@ -56,44 +58,103 @@ export default function({
     rejectButtonText
 }: Props): React$Element<'ul'> {
     return (
-        <ul className = 'knocking-participants-container'>
-            { participants.map(p => (
-                <li
-                    className = 'knocking-participant'
-                    key = { p.id }>
-                    <Avatar
-                        displayName = { p.name }
-                        size = { 48 }
-                        testId = { `${testIdPrefix}.avatar` }
-                        url = { p.loadableAvatarUrl } />
+        // 6293
+        // <ul className = 'knocking-participants-container'>
+        //     { participants.map(p => (
+        //         <li
+        //             className = 'knocking-participant'
+        //             key = { p.id }>
+        //             <Avatar
+        //                 displayName = { p.name }
+        //                 size = { 48 }
+        //                 testId = { `${testIdPrefix}.avatar` }
+        //                 url = { p.loadableAvatarUrl } />
 
-                    <div className = 'details'>
-                        <span data-testid = { `${testIdPrefix}.name` }>
-                            { p.name }
-                        </span>
-                        { p.email && !HIDDEN_EMAILS.includes(p.email) && (
-                            <span data-testid = { `${testIdPrefix}.email` }>
-                                { p.email }
-                            </span>
-                        ) }
-                    </div>
-                    { <NotificationButton
-                        action = { onApprove }
-                        className = 'primary'
-                        id = 'unmute-button'
-                        participant = { p }
-                        testId = { `${testIdPrefix}.allow` }>
-                        { approveButtonText }
-                    </NotificationButton> }
-                    { <NotificationButton
-                        action = { onReject }
-                        className = 'borderLess'
-                        id = 'dismiss-button'
-                        participant = { p }
-                        testId = { `${testIdPrefix}.reject` }>
-                        { rejectButtonText }
-                    </NotificationButton>}
-                </li>
-            )) }
-        </ul>);
+        //             <div className = 'details'>
+        //                 <span data-testid = { `${testIdPrefix}.name` }>
+        //                     { p.name }
+        //                 </span>
+        //                 { p.email && !HIDDEN_EMAILS.includes(p.email) && (
+        //                     <span data-testid = { `${testIdPrefix}.email` }>
+        //                         { p.email }
+        //                     </span>
+        //                 ) }
+        //             </div>
+        //             { <NotificationButton
+        //                 action = { onApprove }
+        //                 className = 'primary'
+        //                 id = 'unmute-button'
+        //                 participant = { p }
+        //                 testId = { `${testIdPrefix}.allow` }>
+        //                 { approveButtonText }
+        //             </NotificationButton> }
+        //             { <NotificationButton
+        //                 action = { onReject }
+        //                 className = 'borderLess'
+        //                 id = 'dismiss-button'
+        //                 participant = { p }
+        //                 testId = { `${testIdPrefix}.reject` }>
+        //                 { rejectButtonText }
+        //             </NotificationButton>}
+        //         </li>
+        //     )) }
+        // </ul>
+        <div
+                className={_toolboxVisible ? "toolbox-visible" : ""}
+                id="knocking-participant-list"
+            >
+                <span className="title">
+                    {t("lobby.knockingParticipantList")}
+                </span>
+                <ul className="knocking-participants-container">
+                    {_participants.map((p) => (
+                        <li className="knocking-participant" key={p.id}>
+                            {infoConf.getIsSecretRoom() ? (
+                                <img src={p.image} />
+                            ) : null}
+                            <div style={{ display: "flex" }}>
+                                <div className="details">
+                                    <span data-testid="knockingParticipant.name">
+                                        {p.name}
+                                    </span>
+                                    {p.email &&
+                                        !HIDDEN_EMAILS.includes(p.email) && (
+                                            <span data-testid="knockingParticipant.email">
+                                                {p.email}
+                                            </span>
+                                        )}
+                                </div>
+                                <button
+                                    className="primary"
+                                    data-testid="lobby.allow"
+                                    onClick={() =>
+                                        this._onRespondToParticipantSocket(
+                                            p.id,
+                                            p.name,
+                                            true
+                                        )
+                                    }
+                                    type="button"
+                                >
+                                    {t("lobby.allow")}
+                                </button>
+                                <button
+                                    className="borderLess"
+                                    data-testid="lobby.reject"
+                                    onClick={() =>
+                                        this._onRespondToParticipantSocket(
+                                            p.id,
+                                            p.name,
+                                            false
+                                        )
+                                    }
+                                    type="button"
+                                >
+                                    {t("lobby.reject")}
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>);
 }
