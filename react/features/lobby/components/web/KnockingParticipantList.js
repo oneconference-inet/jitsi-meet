a7ea8ad6532bd2bec8fa138a56bd2d5e1206d6f7
MaMookMik <mookjrtv3@gmail.com>
2021-09-30 20:12:05 +0700

    test patic list

--- react/features/lobby/components/web/KnockingParticipantList.js
+++ react/features/lobby/components/web/KnockingParticipantList.js
@@ -7,8 +7,6 @@ import { translate } from "../../../base/i18n";
 import { connect } from "../../../base/redux";
 import { isToolboxVisible } from "../../../toolbox/functions.web";
 import { HIDDEN_EMAILS } from "../../constants";
-import NotificationWithParticipants from '../../../notifications/components/web/NotificationWithParticipants';
-import { approveKnockingParticipant, rejectKnockingParticipant } from '../../actions';
 import AbstractKnockingParticipantList, {
     mapStateToProps as abstractMapStateToProps,
     type Props as AbstractProps,
@@ -20,7 +18,7 @@ type Props = AbstractProps & {
     /**
      * True if the toolbox is visible, so we need to adjust the position.
      */
-    _toolboxVisible: boolean
+    _toolboxVisible: boolean,
 };
 
 /**
@@ -33,7 +31,7 @@ class KnockingParticipantList extends AbstractKnockingParticipantList<Props> {
      * @inheritdoc
      */
     render() {
-        const { _participants, _visible, t } = this.props;
+        const { _participants, _toolboxVisible, _visible, t } = this.props;
 
         if (!_visible) {
             return null;
@@ -102,34 +100,22 @@ class KnockingParticipantList extends AbstractKnockingParticipantList<Props> {
                         </li>
                     ))}
                 </ul>
-            // <div id = 'knocking-participant-list'>
-            //     <div className = 'title'>
-            //         { t('lobby.knockingParticipantList') }
-            //     </div>
-            //     <NotificationWithParticipants
-            //         approveButtonText = { t('lobby.allow') }
-            //         onApprove = { approveKnockingParticipant }
-            //         onReject = { rejectKnockingParticipant }
-            //         participants = { _participants }
-            //         rejectButtonText = { t('lobby.reject') }
-            //         testIdPrefix = 'lobby' />
-            // </div>
+            </div>
         );
     }
 }
 
-// /**
-//  * Maps part of the Redux state to the props of this component.
-//  *
-//  * @param {Object} state - The Redux state.
-//  * @returns {Props}
-//  */
-// function _mapStateToProps(state: Object): $Shape<Props> {
-//     return {
-//         ...abstractMapStateToProps(state),
-//         _toolboxVisible: isToolboxVisible(state),
-//     };
-// }
+/**
+ * Maps part of the Redux state to the props of this component.
+ *
+ * @param {Object} state - The Redux state.
+ * @returns {Props}
+ */
+function _mapStateToProps(state: Object): $Shape<Props> {
+    return {
+        ...abstractMapStateToProps(state),
+        _toolboxVisible: isToolboxVisible(state),
+    };
+}
 
-// export default translate(connect(_mapStateToProps)(KnockingParticipantList));
-export default translate(connect(abstractMapStateToProps)(KnockingParticipantList));
+export default translate(connect(_mapStateToProps)(KnockingParticipantList));
