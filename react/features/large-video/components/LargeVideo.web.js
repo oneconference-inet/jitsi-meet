2c36a2e0e494673a869f455c7f55a2a29708c71d
MaMookMik <mookjrtv3@gmail.com>
2021-10-01 12:00:38 +0700

    6293 large video

--- react/features/large-video/components/LargeVideo.web.js
+++ react/features/large-video/components/LargeVideo.web.js
@@ -76,19 +76,14 @@ class LargeVideo extends Component<Props> {
         } = this.props;
         const style = this._getCustomSyles();
         // const className = `videocontainer${this.props._isChatOpen ? ' shift-right' : ''}`;
-        console.log('1111largeVideo');
+        console.log('1111largeVideo1');
 
         return (
-            // 5870
             <div
                 className = 'videocontainer'
                 id = 'largeVideoContainer'
                 style = { style }>
-                <Subject />
-                <InviteMore />
-                <div id = 'sharedVideo'>
-                    <div id = 'sharedVideoIFrame' />
-                </div>
+                <SharedVideo />
                 <div id = 'etherpad' />
 
                 <Watermarks />
@@ -110,9 +105,11 @@ class LargeVideo extends Component<Props> {
                       * another container for the background and the
                       * largeVideoWrapper in order to hide/show them.
                       */}
-                    <div id = 'largeVideoWrapper'>
+                    <div
+                        id = 'largeVideoWrapper'
+                        role = 'figure' >
                         <video
-                            autoPlay = { !this.props._noAutoPlayVideo }
+                            autoPlay = { !_noAutoPlayVideo }
                             id = 'largeVideo'
                             muted = { true }
                             playsInline = { true } /* for Safari on iOS to work */ />
@@ -121,47 +118,6 @@ class LargeVideo extends Component<Props> {
                 { interfaceConfig.DISABLE_TRANSCRIPTION_SUBTITLES
                     || <Captions /> }
             </div>
-
-            // 6293
-            // <div
-            //     className = 'videocontainer'
-            //     id = 'largeVideoContainer'
-            //     style = { style }>
-            //     <SharedVideo />
-            //     <div id = 'etherpad' />
-
-            //     <Watermarks />
-
-            //     <div id = 'dominantSpeaker'>
-            //         <div className = 'dynamic-shadow' />
-            //         <div id = 'dominantSpeakerAvatarContainer' />
-            //     </div>
-            //     <div id = 'remotePresenceMessage' />
-            //     <span id = 'remoteConnectionMessage' />
-            //     <div id = 'largeVideoElementsContainer'>
-            //         <div id = 'largeVideoBackgroundContainer' />
-
-            //         {/*
-            //           * FIXME: the architecture of elements related to the large
-            //           * video and the naming. The background is not part of
-            //           * largeVideoWrapper because we are controlling the size of
-            //           * the video through largeVideoWrapper. That's why we need
-            //           * another container for the background and the
-            //           * largeVideoWrapper in order to hide/show them.
-            //           */}
-            //         <div
-            //             id = 'largeVideoWrapper'
-            //             role = 'figure' >
-            //             <video
-            //                 autoPlay = { !_noAutoPlayVideo }
-            //                 id = 'largeVideo'
-            //                 muted = { true }
-            //                 playsInline = { true } /* for Safari on iOS to work */ />
-            //         </div>
-            //     </div>
-            //     { interfaceConfig.DISABLE_TRANSCRIPTION_SUBTITLES
-            //         || <Captions /> }
-            // </div>
         );
     }
 
