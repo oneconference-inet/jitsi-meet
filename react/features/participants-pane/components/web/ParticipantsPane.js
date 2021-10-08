4b8cd32847a811f15cf9e2392e4c4b2291327376
MaMookMik <mookjrtv3@gmail.com>
2021-10-08 17:59:32 +0700

    test footer pane

--- react/features/participants-pane/components/web/ParticipantsPane.js
+++ react/features/participants-pane/components/web/ParticipantsPane.js
@@ -143,7 +143,7 @@ class ParticipantsPane extends Component<Props, State> {
                         </Container>
                         {_showFooter && (
                             <Footer>
-                                {/* <FooterButton onClick = { this._onMuteAll }>
+                                <FooterButton onClick = { this._onMuteAll }>
                                     {t('participantsPane.actions.muteAll')}
                                 </FooterButton>
                                 <FooterEllipsisContainer>
@@ -152,7 +152,7 @@ class ParticipantsPane extends Component<Props, State> {
                                         onClick = { this._onToggleContext } />
                                     {this.state.contextOpen
                                         && <FooterContextMenu onMouseLeave = { this._onToggleContext } />}
-                                </FooterEllipsisContainer> */}
+                                </FooterEllipsisContainer>
                             </Footer>
                         )}
                     </div>
