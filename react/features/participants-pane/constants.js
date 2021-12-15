ba548f0a215a91b664de665b5531246e06385573
Windtalkers <Windtalkers423@gmail.com>
2021-12-15 11:16:29 +0700

    Fix MediaState, Muted

--- react/features/participants-pane/constants.js
+++ react/features/participants-pane/constants.js
@@ -1,31 +1,39 @@
 // @flow
 
-import React from 'react';
+import React from "react";
 
 import {
     Icon,
     IconCameraEmpty,
     IconCameraEmptyDisabled,
     IconMicrophoneEmpty,
-    IconMicrophoneEmptySlash
-} from '../base/icons';
+    IconMicrophoneEmptySlash,
+} from "../base/icons";
 
 /**
  * Reducer key for the feature.
  */
-export const REDUCER_KEY = 'features/participants-pane';
+export const REDUCER_KEY = "features/participants-pane";
 
-export type ActionTrigger = 'Hover' | 'Permanent'
+export type ActionTrigger = "Hover" | "Permanent";
 
 /**
  * Enum of possible participant action triggers.
  */
-export const ACTION_TRIGGER: {HOVER: ActionTrigger, PERMANENT: ActionTrigger} = {
-    HOVER: 'Hover',
-    PERMANENT: 'Permanent'
+export const ACTION_TRIGGER: {
+    HOVER: ActionTrigger,
+    PERMANENT: ActionTrigger,
+} = {
+    HOVER: "Hover",
+    PERMANENT: "Permanent",
 };
 
-export type MediaState = 'DominantSpeaker' | 'Muted' | 'ForceMuted' | 'Unmuted' | 'None';
+export type MediaState =
+    | "DominantSpeaker"
+    | "Muted"
+    | "ForceMuted"
+    | "Unmuted"
+    | "None";
 
 /**
  * Enum of possible participant media states.
@@ -37,14 +45,14 @@ export const MEDIA_STATE: {
     UNMUTED: MediaState,
     NONE: MediaState,
 } = {
-    DOMINANT_SPEAKER: 'DominantSpeaker',
-    MUTED: 'Muted',
-    FORCE_MUTED: 'ForceMuted',
-    UNMUTED: 'Unmuted',
-    NONE: 'None'
+    DOMINANT_SPEAKER: "DominantSpeaker",
+    MUTED: "Muted",
+    FORCE_MUTED: "ForceMuted",
+    UNMUTED: "Unmuted",
+    NONE: "None",
 };
 
-export type QuickActionButtonType = 'Mute' | 'AskToUnmute' | 'None';
+export type QuickActionButtonType = "Mute" | "AskToUnmute" | "None";
 
 /**
  * Enum of possible participant mute button states.
@@ -52,60 +60,42 @@ export type QuickActionButtonType = 'Mute' | 'AskToUnmute' | 'None';
 export const QUICK_ACTION_BUTTON: {
     MUTE: QuickActionButtonType,
     ASK_TO_UNMUTE: QuickActionButtonType,
-    NONE: QuickActionButtonType
+    NONE: QuickActionButtonType,
 } = {
-    MUTE: 'Mute',
-    ASK_TO_UNMUTE: 'AskToUnmute',
-    NONE: 'None'
+    MUTE: "Mute",
+    ASK_TO_UNMUTE: "AskToUnmute",
+    NONE: "None",
 };
 
 /**
  * Icon mapping for possible participant audio states.
  */
-export const AudioStateIcons: {[MediaState]: React$Element<any> | null} = {
-    [MEDIA_STATE.DOMINANT_SPEAKER]: (
-        <Icon
-            className = 'jitsi-icon-dominant-speaker'
-            size = { 16 }
-            src = { IconMicrophoneEmpty } />
-    ),
-    [MEDIA_STATE.FORCE_MUTED]: (
-        <Icon
-            color = '#E04757'
-            size = { 16 }
-            src = { IconMicrophoneEmptySlash } />
-    ),
+
+console.log(MediaState, "MediaState=>>>>>>");
+console.log(MUTED, "MUTED=>>>>>>");
+
+export const AudioStateIcons: { [MediaState]: React$Element<any> | null } = {
     [MEDIA_STATE.MUTED]: (
         <Icon
-            size = { 16 }
-            src = { IconMicrophoneEmptySlash } />
+            className="jitsi-icon-dominant-speaker"
+            size={16}
+            src={IconMicrophoneEmpty}
+        />
     ),
-    [MEDIA_STATE.UNMUTED]: (
-        <Icon
-            size = { 16 }
-            src = { IconMicrophoneEmpty } />
+    [MEDIA_STATE.FORCE_MUTED]: (
+        <Icon color="#E04757" size={16} src={IconMicrophoneEmptySlash} />
     ),
-    [MEDIA_STATE.NONE]: null
+    [MEDIA_STATE.MUTED]: <Icon size={16} src={IconMicrophoneEmptySlash} />,
+    [MEDIA_STATE.UNMUTED]: <Icon size={16} src={IconMicrophoneEmpty} />,
+    [MEDIA_STATE.NONE]: null,
 };
 
 /**
  * Icon mapping for possible participant video states.
  */
 export const VideoStateIcons = {
-    [MEDIA_STATE.FORCE_MUTED]: (
-        <Icon
-            size = { 16 }
-            src = { IconCameraEmptyDisabled } />
-    ),
-    [MEDIA_STATE.MUTED]: (
-        <Icon
-            size = { 16 }
-            src = { IconCameraEmptyDisabled } />
-    ),
-    [MEDIA_STATE.UNMUTED]: (
-        <Icon
-            size = { 16 }
-            src = { IconCameraEmpty } />
-    ),
-    [MEDIA_STATE.NONE]: null
+    [MEDIA_STATE.FORCE_MUTED]: <Icon size={16} src={IconCameraEmptyDisabled} />,
+    [MEDIA_STATE.MUTED]: <Icon size={16} src={IconCameraEmptyDisabled} />,
+    [MEDIA_STATE.UNMUTED]: <Icon size={16} src={IconCameraEmpty} />,
+    [MEDIA_STATE.NONE]: null,
 };
