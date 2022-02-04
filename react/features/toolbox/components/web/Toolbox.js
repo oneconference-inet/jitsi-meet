9e9dd5ae781659704959b7048a40abcaed84bcc4
seriphap.so <seriphap.so@mandala.co.th>
2022-02-04 15:19:26 +0700

    add log checkservice

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -478,8 +478,8 @@ class Toolbox extends Component<Props, State> {
 
         const isModerator = infoConf.getIsModerator();
         const checkPlatform = infoConf.getService();
-        logger.log("checkPlatform: ",checkPlatform);
         const Checkservice = infoConf.setServiceCheck();
+        logger.log("Checkservice: ",Checkservice);
         if (Checkservice === "onemeeting") {
             dispatch(openParticipantsPane());
         }
