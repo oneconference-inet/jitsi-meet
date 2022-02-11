6eec73b4b71c5d61587ba94ea66cc3735a0189cf
seriphap.so <seriphap.so@mandala.co.th>
2022-02-11 17:13:34 +0700

    try fix ptct btn

--- react/features/participants-pane/components/web/InviteButton.js
+++ react/features/participants-pane/components/web/InviteButton.js
@@ -1,48 +1,32 @@
 // @flow
 
-import React, { useCallback } from "react";
-import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
+import React, { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
 
-import { createToolbarEvent, sendAnalytics } from "../../../analytics";
-import { Icon, IconInviteMore } from "../../../base/icons";
-import { beginAddPeople } from "../../../invite";
-import infoConf from "../../../../../infoConference";
-import { ParticipantInviteButton } from "./styled";
+import { createToolbarEvent, sendAnalytics } from '../../../analytics';
+import { Icon, IconInviteMore } from '../../../base/icons';
+import { beginAddPeople } from '../../../invite';
 
-
+import { ParticipantInviteButton } from './styled';
 
 export const InviteButton = () => {
     const dispatch = useDispatch();
     const { t } = useTranslation();
 
     const onInvite = useCallback(() => {
-        sendAnalytics(createToolbarEvent("invite"));
+        sendAnalytics(createToolbarEvent('invite'));
         dispatch(beginAddPeople());
-    }, [dispatch]);
-    const typeOption = infoConf.getVoice();
+    }, [ dispatch ]);
 
     return (
-        <div>
-            {typeOption == true ? (
-                <div style={{ display: "none" }}>
-                    <ParticipantInviteButton
-                        aria-label={t("participantsPane.actions.invite")}
-                        onClick={onInvite}
-                    >
-                        <Icon size={20} src={IconInviteMore} />
-                        <span>{t("participantsPane.actions.invite")}</span>
-                    </ParticipantInviteButton>
-                </div>
-            ) : (
-                <ParticipantInviteButton
-                    aria-label={t("participantsPane.actions.invite")}
-                    onClick={onInvite}
-                >
-                    <Icon size={20} src={IconInviteMore} />
-                    <span>{t("participantsPane.actions.invite")}</span>
-                </ParticipantInviteButton>
-            )}
-        </div>
+        <ParticipantInviteButton
+            aria-label = { t('participantsPane.actions.invite') }
+            onClick = { onInvite }>
+            <Icon
+                size = { 20 }
+                src = { IconInviteMore } />
+            <span>{t('participantsPane.actions.invite')}</span>
+        </ParticipantInviteButton>
     );
 };
