312931a5f5731f026dbc401d86f7e55b40cef42a
LAPTOP-FTP1L8B0\Windtalkers <chodsukkasem.pi@mandala.co.th>
2022-02-14 11:29:15 +0700

    comment logger

--- react/features/base/tracks/functions.js
+++ react/features/base/tracks/functions.js
@@ -456,7 +456,7 @@ export function isLocalCameraTrackMuted(tracks) {
  */
 export function isLocalTrackMuted(tracks, mediaType) {
     const track = getLocalTrack(tracks, mediaType);
-    logger.log('trackLocalMuted=>>>>',track)
+    // logger.log('trackLocalMuted=>>>>',track)
 
     return !track || track.muted;
 }
@@ -517,7 +517,7 @@ export function isUserInteractionRequiredForUnmute(state) {
  */
 export function setTrackMuted(track, muted) {
     muted = Boolean(muted); // eslint-disable-line no-param-reassign
-    logger.log(track,'settrackMuted=>>>>>>')
+    // logger.log(track,'settrackMuted=>>>>>>')
 
     if (track.isMuted() === muted) {
         return Promise.resolve();
