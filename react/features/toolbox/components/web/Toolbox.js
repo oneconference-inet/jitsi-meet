ed6f22c78a116f68b5c2ae70b0168e752f60d246
LAPTOP-FTP1L8B0\Windtalkers <chodsukkasem.pi@mandala.co.th>
2022-02-24 10:29:18 +0700

    Make Value

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -872,36 +872,6 @@ class Toolbox extends Component<Props, State> {
             _reactionsEnabled,
         } = this.props;
 
-        // logger.log(this.props,'props getAllButtons=>>>>>')
-
-        // logger.log(this.props._localVideo, "props LOcalVideo=>>>>");
-        // logger.log(this.props._localVideo.muted,'this.props._localVideo.muted=>>>>>')
-
-        // const ActionVid = infoConf.setSocket();
-        // var ActionVid = '-actionVid'
-        // logger.log(ActionVid, "ActionVid=>>>>>");
-
-        // if ((ActionVid == "-actionVid")) {
-        //     muteLocalAudio(mute);
-        //     muteLocalVideo(mute);
-        // } else {
-        //     APP.store.getState()["features/base/tracks"];
-        // }
-
-        // if (ActionVid = '-actionVid') {
-        //     this.props._localVideo.muted = true
-        // } else {
-        //     this.props._localVideo.muted = false
-        // }
-
-        // if (ActionVid = '-actionVid') {
-        //     localVideo.muted = true
-        //     return localVideo.muted
-        // } else {
-        //     localVideo = getLocalVideoTrack(state["features/base/tracks"]);
-
-        // }
-
         const microphone = {
             key: "microphone",
             Content: AudioSettingsButton,
